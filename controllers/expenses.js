36c8d047ec902d3ed46373de9f6a064016b03b03
BahaaAY <bahaa.abouyassine2@gmail.com>
2023-11-28 11:09:55 +0200

    Expenses Management

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -2,6 +2,10 @@ const FrequentPurchasesItem = require('../models/purchases_item');
 
 const Purchase = require('../models/purchase');
 const PurchasedItem = require('../models/purchased_item');
+
+const { Op } = require('sequelize');
+
+
 exports.getFrequentPurchases = (req, res, next) => {
     FrequentPurchasesItem.findAll()
         .then((frequentPurchases) => {
@@ -80,6 +84,20 @@ exports.postPurchase = async (req, res, next) => {
 
 };
 
+exports.deletePurchase = async (req, res, next) => {
+    const purchaseId = req.params.purchaseId;
+    try {
+        const purchase = await Purchase.findByPk(purchaseId);
+        if (!purchase) {
+            return res.status(404).json({ message: "Purchase not found" });
+        }
+        await purchase.destroy();
+        res.status(200).json({ message: "Purchase deleted" });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 exports.deleteFrequentPurchase = async (req, res, next) => {
     const freqId = req.params.freqId;
     try {
@@ -93,6 +111,127 @@ exports.deleteFrequentPurchase = async (req, res, next) => {
         console.log(err);
     }
 };
+
+
+//generating reports
+//daily report
+exports.getPurchasesReport = async (req,res,next) => 
+{
+    //Order -> Purchase
+//OrderItem -> PurchasedItem
+//Item -> PurchasesItem(FrequentItem)
+    const type = req.params.type; //daily or monthly or yearly
+    console.log("type: ", type);
+    switch(type)
+    {
+        case "daily":
+            const date = new Date(req.body.date);
+            const startOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+            const endOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+            console.log("body date: ", req.body.date);
+            console.log("date: ", date);
+            try {
+                const purchases = await Purchase.findAll({
+                    where: {
+                        purchase_date: {
+                        [Op.between]: [startOfDay, endOfDay],
+                      },
+                    },
+                    include: [
+                      {
+                        model: PurchasedItem,
+                        include: [
+                          {
+                            model: FrequentPurchasesItem,
+                          },
+                        ],
+                      },
+                    ],
+                  })
+                const total = await Purchase.sum('purchase_total', {
+                    where: {
+                      purchase_date: {
+                        [Op.between]: [startOfDay, endOfDay],
+                      },
+                    },
+                  });
+                res.status(200).json({ count: purchases.length, total: total, purchases: purchases });
+            } catch (err) {
+                console.log(err);
+            }
+            break;
+        case "monthly":
+            const monthIndex = req.body.month-1;
+            const year = req.body.year;
+            const startOfMonth = new Date(year, monthIndex, 1);
+            const endOfMonth = new Date(year, monthIndex + 1, 1);
+            console.log("startOfMonth: ", startOfMonth);
+            console.log("endOfMonth: ", endOfMonth);
+            try {
+                const orders = await Order.findAll({
+                    where: {
+                      order_date: {
+                        [Op.between]: [startOfMonth, endOfMonth],
+                      },
+                    },
+                    include: [
+                      {
+                        model: OrderItem,
+                        include: [
+                          {
+                            model: Item,
+                            include: [
+                              {
+                                model: Category,
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  })
+                const total = await Order.sum('order_total', {
+                    where: {
+                      order_date: {
+                        [Op.between]: [startOfMonth, endOfMonth],
+                      },
+                    },
+                  });
+
+                const days = [];
+                //get the number of days in the month 28 or 29 or 30 or 31
+                const daysInMonth = new Date(year, monthIndex + 1, 0).getDate();
+
+                // sum sales for each day and store them as Day{date , total}
+                for (let i = 1; i <= daysInMonth; i++) {
+                    const startOfDay = new Date(year, monthIndex, i);
+                    const endOfDay = new Date(year, monthIndex, i + 1);
+                    var day_total = await Order.sum('order_total', {
+                        where: {
+                          order_date: {
+                            [Op.between]: [startOfDay, endOfDay],
+                          },
+                        },
+                      });
+                      if(day_total == null){
+                        day_total = 0;
+                      }
+                    days.push({date: (year+'-'+(monthIndex+1)+'-'+i), total: day_total});
+                }
+                res.status(200).json({ count: orders.length, total: total, orders: orders, days: days });
+            } catch (err) {
+                console.log(err);
+            }
+            break;
+
+    }
+
+
+}
+
+
+
+
 
 
 
--- routes/expenses.js
+++ routes/expenses.js
@@ -6,6 +6,10 @@ const expensesController = require("../controllers/expenses");
 router.get("/frequentPurchases", expensesController.getFrequentPurchases);
 router.post("/purchase", expensesController.postPurchase);
 
+router.delete("/purchase/:purchaseId", expensesController.deletePurchase);
+
 router.delete("/frequentPurchases/:freqId", expensesController.deleteFrequentPurchase);
+
+router.post("/expenses-report/:type",expensesController.getPurchasesReport);
 
 module.exports = router;
